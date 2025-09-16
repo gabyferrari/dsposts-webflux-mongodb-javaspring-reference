93d87411458e25a311a97a76c22f3ef66393de30
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-16 20:57:33 -0300

    GET all users + user by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.devsuperior.workshopmongo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -12,20 +19,18 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
-
-	/*
+	
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll() {
-		List<UserDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
-
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		UserDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
+	public Flux<UserDTO> findAll() {
+		return service.findAll();
 	}
 	
+	@GetMapping(value = "/{id}")
+	public Mono<ResponseEntity<UserDTO>> findById(@PathVariable String id) {
+		return service.findById(id).map(userDto -> ResponseEntity.ok().body(userDto));
+	}
+
+	/*
 	@GetMapping(value = "/{id}/posts")
 	public ResponseEntity<List<PostDTO>> findPosts(@PathVariable String id) {
 		List<PostDTO> list = service.findPosts(id);
