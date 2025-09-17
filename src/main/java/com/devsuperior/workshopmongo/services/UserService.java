74be42896fa70c3cd95c1eb7e23e26e138c08fe0
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-16 21:06:09 -0300

    POST new user

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
+import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
@@ -24,6 +25,19 @@ public class UserService {
 		return repository.findById(id).map(existingUser -> new UserDTO(existingUser)) 
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
+	
+	public Mono<UserDTO> insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		Mono<UserDTO> result = repository.save(entity).map(user -> new UserDTO(user));
+		return result;
+	}
+	
+
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+	}
 
 	/*
 	@Transactional(readOnly = true)
@@ -31,14 +45,6 @@ public class UserService {
 		User user = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		List<PostDTO> result = user.getPosts().stream().map(x -> new PostDTO(x)).toList();
 		return result;
-	}
-
-	@Transactional
-	public UserDTO insert(UserDTO dto) {
-		User entity = new User();
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
 	}
 
 	@Transactional
@@ -55,11 +61,6 @@ public class UserService {
 		User entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		repository.delete(entity);
-	}
-
-	private void copyDtoToEntity(UserDTO dto, User entity) {
-		entity.setName(dto.getName());
-		entity.setEmail(dto.getEmail());
 	}
 	*/
 }
