a808c5985f271a784a0f1554fca9dbd39a184411
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-16 21:42:54 -0300

    GET post full search

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopmongo.controllers;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -35,9 +37,8 @@ public class PostController {
 		return service.findByTitle(text);
 	}
 
-	/*
 	@GetMapping(value = "/fullsearch")
-	public ResponseEntity<List<PostDTO>> fullSearch(
+	public Flux<PostDTO> fullSearch(
 			@RequestParam(value = "text", defaultValue = "") String text,
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
@@ -46,7 +47,6 @@ public class PostController {
 		Instant min = URL.convertDate(minDate, Instant.EPOCH);
 		Instant max = URL.convertDate(maxDate, Instant.now());
 		
-		List<PostDTO> list = service.fullSearch(text, min, max);
-		return ResponseEntity.ok(list);
-	*/
+		return service.fullSearch(text, min, max);
+	}
 }
--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +27,8 @@ public class PostService {
 		return repository.searchTitle(text).map(postFound -> new PostDTO(postFound));
 	}
 
-	/*
-	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
+	public Flux<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
 		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
-		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
-		return result;
+		return repository.fullSearch(text, minDate, maxDate).map(postFound -> new PostDTO(postFound));
 	}
-	*/
 }
